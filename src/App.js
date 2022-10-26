e63a1b441ae9b2b91574025c7e55942124a6d8ca
dev1ninja <devninja.up@gmail.com>
2022-10-26 11:30:08 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { incrementBird, addBird } from './store/birds/birds';
 import './App.css';
 
 function App() {
-  const birds = useSelector(state => state.birds);
+  const birds = [...useSelector(state => state.birds)].sort((a, b) => {
+    return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1;
+  })
+  const dispatch = useDispatch();
+
+  const [birdName, setBird] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    dispatch(addBird(birdName))
+    setBird('');
+  }
+
   return (
     <div className='wrapper'>
       <h1>Bird List</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <p>
             Add Bird
           </p>
-          <input type={"text"} />
+          <input type={"text"} onChange={e => setBird(e.target.value)} value={birdName} />
         </label>
         <div>
           <button type='submit'>Add</button>
@@ -24,7 +37,7 @@ function App() {
             <h3>{bird.name}</h3>
             <div>
               Views: {bird.views}
-              <button><span role="img" aria-label='add'>➕</span></button>
+              <button onClick={() => dispatch(incrementBird(bird.name))}><span role="img" aria-label='add'>➕</span></button>
             </div>
           </li>
         ))}
