49b2ebae5c1c506238d07a9d9ac566d925e0dc61
dev1ninja <devninja.up@gmail.com>
2022-10-26 11:08:26 -0500

    add action

--- src/index.js
+++ src/index.js
@@ -5,15 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import birdApp from './store/birds/birds';
 
-const store = createStore(() => ({
-  birds: [
-    {
-      name: 'robin',
-      views: 1
-    }
-  ]
-}));
+const store = createStore(birdApp);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
