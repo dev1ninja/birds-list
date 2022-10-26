6125281d51dde613ab15b44f0d654fe1b9e60763
dev1ninja <devninja.up@gmail.com>
2022-10-26 10:29:31 -0500

    connect store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+  const birds = useSelector(state => state.birds);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <h1>Bird List</h1>
+      <form>
+        <label>
+          <p>
+            Add Bird
+          </p>
+          <input type={"text"} />
+        </label>
+        <div>
+          <button type='submit'>Add</button>
+        </div>
+      </form>
+      <ul>
+        {birds.map(bird => (
+          <li key={bird.name}>
+            <h3>{bird.name}</h3>
+            <div>
+              Views: {bird.views}
+              <button><span role="img" aria-label='add'>➕</span></button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(() => ({
+  birds: [
+    {
+      name: 'robin',
+      views: 1
+    }
+  ]
+}));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
